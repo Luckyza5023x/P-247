5fb7334343b0dd58f46a17527a6e312aa0cf8acc
Luckyza5023x <113436103+Luckyza5023x@users.noreply.github.com>
2023-12-19 20:01:39 +0530

    Add files via upload

---
+++ ESP32_RGB_controller_bp.ino
@@ -0,0 +1,147 @@
+// To connect with WiFi
+#include<WiFi.h>
+
+//  To make our device as server 
+#include<WebServer.h>
+
+//  ssid , password
+const char ssid[] = "Your wifi ssid";
+const char password[] = "your password";
+
+void handleRoot(){
+// (192.168.1.1/){
+  server.send(200, "text/html", button); //here 200 is Success code 
+}
+
+//  RGb led pins
+byte r_pin = 25;
+byte g_pin = 26;
+byte b_pin = 27;
+
+//  defining channels
+#define r_channel 0
+#define g_channel 1
+#define b_channel 2
+
+//  defining frequency and reolution
+#define frequency 5000
+#define resolution 8
+
+//  starting a server at port 80
+WebServer server(80);
+
+void handleNotFound() {
+  
+  String message = "File Not Found\n\n";
+  message += "URI: "; // (192.168.1.1/page1/on)
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+ 
+}
+
+void setup(){
+
+  //  setting frequency and resolution at channels
+  ledcSetup(r_channel , frequency , resolution);
+  ledcSetup(g_channel , frequency , resolution);
+  ledcSetup(b_channel , frequency , resolution);
+
+  //  attaching GPIOs
+  ledcAttachPin(r_pin , r_channel);
+  ledcAttachPin(g_pin , g_channel);
+  ledcAttachPin(b_pin , b_channel);
+
+  //   starting serial communication
+  Serial.begin(115200);
+
+  //  connecting with the WiFi and printing IP Address
+  Serial.print("Connecting with : ");
+  Serial.println(ssid);
+  WiFi.begin(ssid , password);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("Connected with the Network !");
+  Serial.print("IP address assgined by the access point : ");
+  Serial.println(WiFi.localIP());
+
+  //  Define APIs
+
+
+  
+  
+
+  //  Define API, if other APIs are not found
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("Http server started");
+  digitalWrite(LED_1, LOW);
+
+  
+  
+
+  //  start the server
+
+  server.on("/", handleRoot);
+   
+
+  
+  Serial.println("HTTP Server started");
+  
+}
+
+void loop(){
+
+  server.handleClient();
+  
+
+}
+
+void color_generator(byte r , byte g , byte b){
+  ledcWrite(r_channel , r);
+  ledcWrite(g_channel , g);
+  ledcWrite(b_channel , b);
+}
+
+
+//  generating HTML Page
+String generate_html(){
+  
+  String html = "<!DOCTYPE html>\n";
+         html += "<html lang=\"en\">\n";
+         html += "<head>\n";
+         html += "<meta charset=\"UTF-8\">\n";
+         html += "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
+         html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+         html += "<title>ESP32 RGB CONTROLLER</title>\n";
+         html += "</head>\n";
+         html += "<body>\n";
+         html += "<div style=\"padding-left: 50%; padding-top: 10%;\">\n";
+         html += "<h1>COLOR GENERATOR</h1>\n";
+         html += "</div>\n";
+
+         html += "<div style=\"padding-left: 40%;\">\n";
+         html += "<a href=\"/red\"><button>Red</button></a>\n";
+         html += "<a href=\"/green\"><button>Green</button></a>\n";
+         html += "<a href=\"/blue\"><button>Blue</button></a>\n";
+         html += "<a href=\"/yellow\"><button>Yellow</button></a>\n";
+         html += "<a href=\"/cyan\"><button>Cyan</button></a>\n";
+         html += "<a href=\"/magenta\"><button>Magenta</button></a>\n";
+         html += "<a href=\"/orange\"><button>Orange</button></a>\n";
+         html += "</div>\n";
+    
+         html += "</body>\n";
+         html += "</html>\n";
+
+  return html;
+}
